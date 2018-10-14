94eb55109cd461ba48f8a95e5aa9d5a55a65d365
Lehner Florian <dev@der-flo.net>
2018-10-14 18:18:44 +0200

    Update example
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- example_test.go
+++ example_test.go
@@ -1,8 +1,11 @@
+//+build linux
+
 package nflog_test
 
 import (
 	"context"
 	"fmt"
+	"time"
 
 	nfl "github.com/florianl/go-nflog"
 	"golang.org/x/sys/unix"
@@ -19,11 +22,19 @@ func ExampleNflog_Register() {
 	}
 	defer nflog.Close()
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	// Register your function to listen on nflog group 100
-	err = nflog.Register(context.Background(), unix.AF_INET, 100, nfl.NfUlnlCopyPacket,
+	err = nflog.Register(ctx, unix.AF_INET, 100, nfl.NfUlnlCopyPacket,
 		func(m nfl.Msg) int { fmt.Printf("%#v\n", m); return 0 })
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+
+	select {
+	// Block till the context expires
+	case <-ctx.Done():
 	}
 }
