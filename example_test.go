541f08421ff3f795be417957b3b9b3b33318bd62
Lehner Florian <dev@der-flo.net>
2018-10-18 19:35:23 +0200

    Update example
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- example_test.go
+++ example_test.go
@@ -7,27 +7,35 @@ import (
 	"fmt"
 	"time"
 
-	nfl "github.com/florianl/go-nflog"
+	"github.com/florianl/go-nflog"
 	"golang.org/x/sys/unix"
 )
 
 func ExampleNflog_Register() {
 	// Send outgoing pings to nflog group 100
-	// #iptables -I OUTPUT -p icmp -j NFLOG --nflog-group 100
+	// # sudo iptables -I OUTPUT -p icmp -j NFLOG --nflog-group 100
 
-	nflog, err := nfl.Open()
+	nf, err := nflog.Open()
 	if err != nil {
 		fmt.Println("could not open nflog socket:", err)
 		return
 	}
-	defer nflog.Close()
+	defer nf.Close()
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
+	fn := func(m nflog.Msg) int {
+		ts, err := m.Timestamp()
+		if err != nflog.ErrNoTimestamp {
+			ts = time.Now()
+		}
+		fmt.Printf("%s\t%v\n", ts, m[nflog.NfUlaAttrPayload])
+		return 0
+	}
+
 	// Register your function to listen on nflog group 100
-	err = nflog.Register(ctx, unix.AF_INET, 100, nfl.NfUlnlCopyPacket,
-		func(m nfl.Msg) int { fmt.Printf("%#v\n", m); return 0 })
+	err = nf.Register(ctx, unix.AF_INET, 100, nflog.NfUlnlCopyPacket, fn)
 	if err != nil {
 		fmt.Println(err)
 		return
