b1bfa6bb65ddc6975d4630c54cb62a44b259ca38
Lehner Florian <dev@der-flo.net>
2018-12-06 20:29:59 +0100

    Use new attribute decoder
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- attribute.go
+++ attribute.go
@@ -12,42 +12,70 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// Timestamp returns the timestamp of the message
-func (m *Msg) Timestamp() (time.Time, error) {
-	/*
-		struct nfulnl_msg_packet_timestamp {
-		__aligned_be64	sec;
-		__aligned_be64	usec;
-		};
-	*/
-	var sec, usec int64
-	data := (*m)[NfUlaAttrTimestamp]
-	if len(data) == 0 {
-		return time.Unix(0, 0), ErrNoTimestamp
-	}
-	r := bytes.NewReader(data[:8])
-	if err := binary.Read(r, binary.BigEndian, &sec); err != nil {
-		return time.Unix(0, 0), err
-	}
-	r = bytes.NewReader(data[8:])
-	if err := binary.Read(r, binary.BigEndian, &usec); err != nil {
-		return time.Unix(0, 0), err
-	}
-	return time.Unix(sec, usec*1000), nil
-}
-
 func extractAttribute(m Msg, logger *log.Logger, data []byte) error {
-	attributes, err := netlink.UnmarshalAttributes(data)
+	ad, err := netlink.NewAttributeDecoder(data)
 	if err != nil {
 		return err
 	}
-
-	for _, attr := range attributes {
-		if int(attr.Type) >= attrMax || int(attr.Type) == attrUnspec {
-			logger.Fatalf("extractAttribute(): %d %d %v\n", attr.Type, attr.Length, attr.Data)
-			continue
+	ad.ByteOrder = binary.BigEndian
+	for ad.Next() {
+		switch ad.Type() {
+		case nfUlaAttrPacketHdr:
+			m[AttrHwProtocol] = binary.BigEndian.Uint16(ad.Bytes()[:2])
+			m[AttrHook] = ad.Bytes()[3]
+		case nfUlaAttrMark:
+			m[AttrMark] = ad.Bytes()
+		case nfUlaAttrTimestamp:
+			var sec, usec int64
+			r := bytes.NewReader(ad.Bytes()[:8])
+			if err := binary.Read(r, binary.BigEndian, &sec); err != nil {
+				return err
+			}
+			r = bytes.NewReader(ad.Bytes()[8:])
+			if err := binary.Read(r, binary.BigEndian, &usec); err != nil {
+				return err
+			}
+			m[AttrTimestamp] = time.Unix(sec, usec*1000)
+		case nfUlaAttrIfindexIndev:
+			m[AttrIfindexIndev] = ad.Uint32()
+		case nfUlaAttrIfindexOutdev:
+			m[AttrIfindexOutdev] = ad.Uint32()
+		case nfUlaAttrIfindexPhysIndev:
+			m[AttrIfindexPhysIndev] = ad.Uint32()
+		case nfUlaAttrIfindexPhysOutdev:
+			m[AttrIfindexPhysOutdev] = ad.Uint32()
+		case nfUlaAttrHwaddr:
+			hwAddrLen := binary.BigEndian.Uint16(ad.Bytes()[:2])
+			m[AttrHwAddr] = (ad.Bytes())[4 : 4+hwAddrLen]
+		case nfUlaAttrPayload:
+			m[AttrPayload] = ad.Bytes()
+		case nfUlaAttrPrefix:
+			m[AttrPrefix] = ad.String()
+		case nfUlaAttrUID:
+			m[AttrUID] = ad.Uint32()
+		case nfUlaAttrSeq:
+			m[AttrSeq] = ad.Uint32()
+		case nfUlaAttrSeqGlobal:
+			m[AttrSeqGlobal] = ad.Uint32()
+		case nfUlaAttrGID:
+			m[AttrGID] = ad.Uint32()
+		case nfUlaAttrHwType:
+			m[AttrHwType] = ad.Uint16()
+		case nfUlaAttrHwHeader:
+			m[AttrHwHeader] = ad.Bytes()
+		case nfUlaAttrHwLen:
+			m[AttrHwLen] = ad.Uint16()
+		case nfUlaAttrCt + nlafNested:
+			m[AttrCt] = ad.Bytes()
+		case nfUlaAttrCtInfo:
+			m[AttrCtInfo] = ad.Uint32()
+		default:
+			logger.Printf("Unknown attribute: %d %v\n", ad.Type(), ad.Bytes())
 		}
-		m[int(attr.Type)] = attr.Data
+	}
+
+	if err := ad.Err(); err != nil {
+		return err
 	}
 	return nil
 }
@@ -60,7 +88,7 @@ func checkHeader(data []byte) int {
 }
 
 func extractAttributes(logger *log.Logger, msg []byte) (Msg, error) {
-	var data = make(map[int][]byte)
+	var data = make(Msg)
 
 	offset := checkHeader(msg[:2])
 	if err := extractAttribute(data, logger, msg[offset:]); err != nil {
