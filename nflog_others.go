d7ab8fe58ce331eada8849a4f8df994edba9efc0
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2018-12-07 05:58:47 +0100

    Update non-Linux stubs with latest API change

--- nflog_others.go
+++ nflog_others.go
@@ -5,6 +5,7 @@ package nflog
 import (
 	"context"
 	"errors"
+	"log"
 )
 
 var errNotLinux = errors.New("Not implemented for OS other than linux")
@@ -12,11 +13,17 @@ var errNotLinux = errors.New("Not implemented for OS other than linux")
 // Nflog is not implemented for OS other than linux
 type Nflog struct{}
 
+// Config contains options for a Conn.
+type Config struct {
+	// Interface to log internals.
+	Logger *log.Logger
+}
+
 // Msg contains all the information of a connection
 type Msg map[int][]byte
 
 // Open is not implemented for OS other than Linux
-func Open() (*Nflog, error) {
+func Open(config *Config) (*Nflog, error) {
 	return nil, errNotLinux
 }
 
