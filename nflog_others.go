bbf60c7e894c7b7030378ce93165ebcb489bee5a
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2018-12-11 07:18:45 +0100

    Update non-Linux stubs with latest API change (Config struct)

--- nflog_others.go
+++ nflog_others.go
@@ -15,6 +15,32 @@ type Nflog struct{}
 
 // Config contains options for a Conn.
 type Config struct {
+	// Network namespace the Nflog needs to operate in. If set to 0 (default),
+	// no network namespace will be entered.
+	NetNS int
+
+	// Optional flags
+	Flags uint16
+
+	// Specifies the number of packets in the group,
+	// until they will be pushed to userspace.
+	QThresh uint32
+
+	// Maximum time in 1/100s that a packet in the nflog group will be queued,
+	// until it is pushed to userspace.
+	Timeout uint32
+
+	// Nflog group this socket will be assigned to.
+	Group uint16
+
+	// Specifies how the kernel handles a packet in the nflog group.
+	Copymode uint8
+
+	// If NfUlnlCopyPacket is set as CopyMode,
+	// this parameter specifies the maximum number of bytes,
+	// that will be copied to userspace.
+	Bufsize uint32
+
 	// Interface to log internals.
 	Logger *log.Logger
 }
@@ -23,7 +49,7 @@ type Config struct {
 type Msg map[int][]byte
 
 // Open is not implemented for OS other than Linux
-func Open(config *Config) (*Nflog, error) {
+func Open(_ *Config) (*Nflog, error) {
 	return nil, errNotLinux
 }
 
@@ -36,6 +62,6 @@ func (_ *Nflog) Close() error {
 type HookFunc func(_ Msg) int
 
 // Register is not implemented for OS other than Linux
-func (_ *Nflog) Register(_ context.Context, _, _ int, _ byte, _ HookFunc) error {
+func (_ *Nflog) Register(_ context.Context, _ HookFunc) error {
 	return errNotLinux
 }
