7311f99e7535881f45d1d314a2b3d46af3f36689
Florian Lehner <florianl@users.noreply.github.com>
2019-09-04 19:37:59 +0200

    fix endianess for some attributes (#8)
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- attribute.go
+++ attribute.go
@@ -26,8 +26,10 @@ func extractAttribute(a *Attribute, logger *log.Logger, data []byte) error {
 			hook := uint8(ad.Bytes()[3])
 			a.Hook = &hook
 		case nfUlaAttrMark:
+			ad.ByteOrder = binary.BigEndian
 			mark := ad.Uint32()
 			a.Mark = &mark
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrTimestamp:
 			var sec, usec int64
 			r := bytes.NewReader(ad.Bytes()[:8])
@@ -41,17 +43,25 @@ func extractAttribute(a *Attribute, logger *log.Logger, data []byte) error {
 			timestamp := time.Unix(sec, usec*1000)
 			a.Timestamp = &timestamp
 		case nfUlaAttrIfindexIndev:
+			ad.ByteOrder = binary.BigEndian
 			inDev := ad.Uint32()
 			a.InDev = &inDev
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrIfindexOutdev:
+			ad.ByteOrder = binary.BigEndian
 			outDev := ad.Uint32()
 			a.OutDev = &outDev
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrIfindexPhysIndev:
+			ad.ByteOrder = binary.BigEndian
 			physInDev := ad.Uint32()
 			a.PhysInDev = &physInDev
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrIfindexPhysOutdev:
+			ad.ByteOrder = binary.BigEndian
 			physOutDev := ad.Uint32()
 			a.PhysOutDev = &physOutDev
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrHwaddr:
 			hwAddrLen := binary.BigEndian.Uint16(ad.Bytes()[:2])
 			hwAddr := (ad.Bytes())[4 : 4+hwAddrLen]
@@ -63,26 +73,38 @@ func extractAttribute(a *Attribute, logger *log.Logger, data []byte) error {
 			prefix := ad.String()
 			a.Prefix = &prefix
 		case nfUlaAttrUID:
+			ad.ByteOrder = binary.BigEndian
 			uid := ad.Uint32()
 			a.UID = &uid
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrSeq:
+			ad.ByteOrder = binary.BigEndian
 			seq := ad.Uint32()
 			a.Seq = &seq
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrSeqGlobal:
+			ad.ByteOrder = binary.BigEndian
 			seqGlobal := ad.Uint32()
 			a.SeqGlobal = &seqGlobal
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrGID:
+			ad.ByteOrder = binary.BigEndian
 			gid := ad.Uint32()
 			a.GID = &gid
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrHwType:
+			ad.ByteOrder = binary.BigEndian
 			hwType := ad.Uint16()
 			a.HwType = &hwType
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrHwHeader:
 			hwHeader := ad.Bytes()
 			a.HwHeader = &hwHeader
 		case nfUlaAttrHwLen:
+			ad.ByteOrder = binary.BigEndian
 			hwLen := ad.Uint16()
 			a.HwLen = &hwLen
+			ad.ByteOrder = nativeEndian
 		case nfUlaAttrCt + nlafNested:
 			ct := ad.Bytes()
 			a.Ct = &ct
