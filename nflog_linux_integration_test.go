99931cb38374d5e6c97bab5718cb2774378b2a42
Lehner Florian <dev@der-flo.net>
2020-11-07 12:43:55 +0100

    use context instead of time.Sleep()

--- nflog_linux_integration_test.go
+++ nflog_linux_integration_test.go
@@ -41,7 +41,9 @@ func TestLinuxNflog(t *testing.T) {
 	<-ctx.Done()
 }
 
-func startNflog(t *testing.T, group uint16) (func(), error) {
+func startNflog(ctx context.Context, t *testing.T, group uint16) (func(), error) {
+	t.Helper()
+
 	config := Config{
 		Group:       group,
 		Copymode:    CopyPacket,
@@ -57,7 +59,7 @@ func startNflog(t *testing.T, group uint16) (func(), error) {
 		return 1
 	}
 
-	err = nf.Register(context.Background(), fn)
+	err = nf.Register(ctx, fn)
 	if err != nil {
 		return func() {}, err
 	}
@@ -67,16 +69,19 @@ func startNflog(t *testing.T, group uint16) (func(), error) {
 
 func TestLinuxMultiNflog(t *testing.T) {
 	var cleanUp []func()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	for i := 32; i <= 42; i++ {
-		function, err := startNflog(t, uint16(i))
+		function, err := startNflog(ctx, t, uint16(i))
 		if err != nil {
 			t.Fatalf("failed to open nflog socket for group %d: %v", i, err)
 		}
 		cleanUp = append(cleanUp, function)
 	}
 
-	time.Sleep(5 * time.Second)
+	// Block till the context expires
+	<-ctx.Done()
 
 	for _, function := range cleanUp {
 		function()
